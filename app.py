d7ec194531a474d7d472fdb066e39e833930acf6
nagumeena <nagumeenaudayappan.23aid@kongu.edu>
2025-07-30 20:30:03 +0530

    Fix: Renamed flask_app to app and bug in predict route

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import numpy as np
 from flask import Flask,request,render_template
 import pickle
 
-flask_app = Flask(__name__)
+app = Flask(__name__)
 model = pickle.load(open("model.pkl","rb"))
 
-@flask_app.route("/")
+@app.route("/")
 
 def home():
     return render_template("index.html")
-@flask_app.route("/predict",methods=["POST"])
+@app.route("/predict",methods=["POST"])
 def predict():
     float_feature= [float(x) for x in request.form. values ()]
     features =[np.array(float_feature)]
@@ -18,4 +18,4 @@ def predict():
     predicted_crop = prediction[0]
     return render_template("index.html", prediction_text="The Predicted Crop is {}".format(predicted_crop))
 if __name__ == "__main__":
-    flask_app.run(debug = True,host='0.0.0.0', port=5000)
+    app.run(debug = True,host='0.0.0.0', port=5000)
